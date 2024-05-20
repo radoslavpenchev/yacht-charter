887dc38f4abe558c3e0da023187b6790e7275598
Radoslav <raro7893@gmail.com>
2024-05-20 21:58:51 +0300

    get_current_user

--- server/dependencies/auth_dependencies.py
+++ server/dependencies/auth_dependencies.py
@@ -1,8 +1,31 @@
-from fastapi import Depends, HTTPException
+from jose import JWTError, jwt
+
+from fastapi import Depends, HTTPException, Header
 from app.models.entities.user import UserEntity
 from app.services.auth_service import AuthService
 from app.services.repositories.user_repository import UserRepository
 from dependencies.repositories_loaders import get_user_repository
+
+SECRET_KEY = "aP2@!0t1*lk#Hjs8F8dl&kL2nP!0t1*LKz!Xb@Qe"
+ALGORITHM = "HS256"
+
+async def get_current_user(authorization: str = Header(...),
+    user_repository: UserRepository = Depends(get_user_repository),
+    ) -> UserEntity:
+    try:
+        token = authorization.split(" ")[1]
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email = payload.get("sub")
+        if email is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+    except (IndexError, JWTError):
+        raise HTTPException(status_code=401, detail="Invalid token")
+    
+    user = user_repository.get_by_email(email=email)
+    if user is None:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    return user
 
 
 async def get_auth_service(
