d0024073bbdb15c4ac6fb5b8dd6fdd7728a9bb84
Radoslav <raro7893@gmail.com>
2024-05-23 19:50:09 +0300

    user login system

--- server/app/main.py
+++ server/app/main.py
@@ -3,6 +3,8 @@ from app.controllers.yacht_controller import yacht_controller
 from app.controllers.port_controller import port_controller
 from app.controllers.reservation_controller import reservation_controller   
 from app.controllers.user_controller import user_controller
+from app.controllers.auth_controller import auth_controller
+
 
 app = FastAPI()
 
@@ -12,3 +14,4 @@ app.include_router(yacht_controller)
 app.include_router(port_controller)
 app.include_router(reservation_controller)
 app.include_router(user_controller)
+app.include_router(auth_controller)
